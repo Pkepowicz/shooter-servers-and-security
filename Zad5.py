b1fac4081cab7b7ff3d41700255b885566107401
Dawid Kowal <dawidkowal0@gmail.com>
2023-05-30 19:02:55 +0200

    minor text changes

--- Zad5.py
+++ Zad5.py
@@ -9,7 +9,7 @@ password = 'dobrehaselko'
 salt = b'DRw\xaaj_\xfb\xca\xec\x91\xcd \xd9xk\x88\xe0\xbd\x97\xcab-\xe9\xa7D ^\xee\x93\xac\x92\x1f'
 key = PBKDF2(password, salt, dkLen=32)
 
-""" Tu pisz swój kod """
+"""Tu pisz swój kod"""
 #cipher = 
 #original_data = 
 """"""
